9d716b121429cd5d573276b1c61967e48c24fed8
Mariano Marzullo <mariano.marzullo@gmail.com>
2013-03-16 00:38:26 -0300

    added lot of features

--- CaloriesCalculator/src/com/traincoders/impl/SplashActivity.java
+++ CaloriesCalculator/src/com/traincoders/impl/SplashActivity.java
@@ -2,16 +2,58 @@ package com.traincoders.impl;
 
 import android.app.*;
 import android.content.*;
+import android.graphics.PixelFormat;
 import android.os.*;
 import android.view.*;
 
 public class SplashActivity extends Activity{
 	
-		private Thread mSplashThread;
-		
-		@Override
-		public void onCreate(Bundle savedInstanceState){
-				super.onCreate(savedInstanceState);  
-				setContentView(R.layout.splash);
-		}
+	// used to know if the back button was pressed in the splash screen activity and avoid opening the next activity
+    private boolean mIsBackButtonPressed;
+    private static final int SPLASH_DURATION = 2000; // 2 seconds
+ 
+ 
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);        
+        Handler handler = new Handler();
+ 
+        // run a thread after 2 seconds to start the home screen
+        handler.postDelayed(new Runnable() {
+ 
+            @Override
+            public void run() {
+ 
+                // make sure we close the splash screen so the user won't come back when it presses back key
+ 
+                finish();
+                 
+                if (!mIsBackButtonPressed) {
+                    // start the home screen if the back button wasn't pressed already 
+                	
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    SplashActivity.this.startActivity(intent);
+               }
+                 
+            }
+ 
+        }, SPLASH_DURATION); // time in milliseconds (1 second = 1000 milliseconds) until the run() method will be called
+ 
+    }
+ 
+    @Override
+   public void onBackPressed() {
+ 
+        // set the flag to true so the next activity won't start up
+        mIsBackButtonPressed = true;
+        super.onBackPressed();
+ 
+    }
+    
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
 }
