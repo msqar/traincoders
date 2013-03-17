e826dc955ce6a3d3df391d693095d96c13400a4b
Mariano Marzullo <mariano.marzullo@gmail.com>
2013-03-17 01:13:02 -0300

    aligning version to percentile

--- CaloriesCalculator/src/com/traincoders/impl/PercentileActivity.java
+++ CaloriesCalculator/src/com/traincoders/impl/PercentileActivity.java
@@ -2,17 +2,19 @@ package com.traincoders.impl;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class PercentileActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.percentile_layout);
-        
-        
+        displayVersionName();        
     }
     
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,5 +34,18 @@ public class PercentileActivity extends Activity {
 			   return true;
 		}
 		return false;
+	}
+	
+	private void displayVersionName() {
+	    String versionName = "";
+	    PackageInfo packageInfo;
+	    try {
+	        packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+	        versionName = "v " + packageInfo.versionName;
+	    } catch (NameNotFoundException e) {
+	        e.printStackTrace();
+	    }
+	    TextView tv = (TextView) findViewById(R.id.app_version);
+	    tv.setText(versionName);
 	}
 }
